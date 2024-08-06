d285eebdc5cd93c991455fa099c1e69267e1caf8
Wojtazz <marcin.witas72@gmail.com>
2024-08-06 21:24:01 +0200

    feat: add Container component

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import Container from '@/components/ui/Container.vue'
+</script>
+
 <template>
   <div class="font-roboto">
-    <main>
-      <slot/>
-    </main>
+    <Container>
+      <main>
+        <slot/>
+      </main>
+    </Container>
   </div>
 </template>
