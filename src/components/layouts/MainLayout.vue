ae59cb657a0d2b516b7abe59822d0dff81b228f4
Wojtazz <marcin.witas72@gmail.com>
2024-08-06 22:08:54 +0200

    refactor: move Container to About section

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import Container from '@/components/ui/Container.vue'
-</script>
-
 <template>
   <div class="font-roboto">
-    <Container>
       <main>
         <slot/>
       </main>
-    </Container>
   </div>
 </template>
