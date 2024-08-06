d16df815f30014c2c6a861612663c2784819e895
Wojtazz <marcin.witas72@gmail.com>
2024-08-06 20:17:50 +0200

    chore: install primevue

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import '@/assets/styles.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import PrimeVue from "primevue/config";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true
+})
 
 app.mount('#app')
