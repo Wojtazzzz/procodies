f3ed5b336c2327bef2cfe185516eeb0c1d51fb84
Wojtazz <marcin.witas72@gmail.com>
2024-08-06 21:05:32 +0200

    feat: add Heading component

---
+++ src/components/ui/Heading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}>()
+</script>
+
+<template>
+  <component :is="type">
+    <slot />
+  </component>
+</template>
