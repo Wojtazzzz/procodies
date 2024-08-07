5780d83d1ddbc79d5909c6a034905dbe79a0d60c
Wojtazz <marcin.witas72@gmail.com>
2024-08-07 17:55:03 +0200

    refactor: improve keyboard navigation through button component

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -12,5 +12,5 @@ defineProps<{
 </script>
 
 <template>
-  <PVButton :type="type" :label="label" class="w-full bg-primary-base text-white font-medium px-4 py-3 rounded-[10px] hover:bg-primary-base/90 active:bg-primary-base/80 transition-colors" />
+  <PVButton :type="type" :label="label" class="w-full bg-primary-base text-white font-medium px-4 py-3 rounded-[10px] hover:bg-primary-base/90 active:bg-primary-base/80 transition-colors focus-visible:outline-2 focus-visible:outline-dashed focus-visible:outline-offset-4" />
 </template>
